5e6fc2b181c667391b042e681d177c59f724282c
Ahlan Rezki <ahlanrezki@gmail.com>
2022-10-09 21:29:52 +0700

    Initial commit

---
+++ src/main/java/ujiandua/Soal1.java
@@ -0,0 +1,26 @@
+package ujiandua;
+
+public class Soal1 {
+        public static void main(String[] args) {
+            for (int i = 1; i < 10; i += 2) {
+                System.out.print(i + "_");
+            }
+            System.out.println("");
+            for (int n = 3; n < 8; n += 2) {
+                System.out.print(n + "_");
+            }
+            System.out.println("");
+            for (int x = 3; x < 8; x += 2) {
+                System.out.print(x + "_");
+            }
+            System.out.println("");
+            for (int u = 5; u < 6; u++) {
+                System.out.print("_" + u + "_");
+            }
+            System.out.println("");
+            for (int u = 5; u < 6; u++) {
+                System.out.print(u);
+            }
+        }
+    }
+
